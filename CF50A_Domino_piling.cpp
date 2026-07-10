f5780019cfc74fd889abfa93965624eb94f41880
Abdullah Ahmad Sadik <74974785+Life932@users.noreply.github.com>
2026-07-10 21:21:28 +0600

    Add domino piling calculation in CF50A_Domino_piling.cpp
    Implement main function to calculate dominoes that can fit in a grid.

---
+++ CF50A_Domino_piling.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    int m, n;
+    
+    cin>> m >> n;
+    
+    cout << m*n/2; //Total area divided by 2, the rest remaining squares are ignored since filling all the squares isn't a precondition. Try to figure out the patterns itself.
+    
+    return 0;
+    
+}
